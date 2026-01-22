fe390f7127e411fb339bd3582c12e68d0123131e
Amish <amishv20@gmail.com>
2026-01-22 11:17:31 +0530

    added arithmetic operations

---
+++ arithmetic.py
@@ -0,0 +1,11 @@
+import math
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print(a + b)
+print(abs(a - b))
+print(a * b)
+print(a / b)
+print(a // b)
+print(a ** b)
