e9c93a9b334abb7c56c24e44dcac4e59b501615f
Amish <amishv20@gmail.com>
2026-01-22 11:30:56 +0530

    added few more simple logics related to arithmetic

--- arithmetic.py
+++ arithmetic.py
@@ -1,11 +1,34 @@
 import math
 
-a = int(input("Enter the first number: "))
+"""a = int(input("Enter the first number: "))
 b = int(input("Enter the second number: "))
 
-print(a + b)
-print(abs(a - b))
-print(a * b)
-print(a / b)
-print(a // b)
-print(a ** b)
+print(a + b)    #Addition
+print(abs(a - b))   #Subtraction
+print(a * b)        #Multiplication
+print(a / b)        #division
+print(a // b)       #division with Absolute value
+print(a ** b)       #Power 
+
+
+
+
+#-----------------------------------------------KM-M converter-----------------------------
+km=float(input("Enter the distance in Km: "))
+print("Distance in meter",km*1000)
+
+
+
+
+
+#--------------------------------------------------------Temp--------------------------------------------------
+temp=float(input("Temperature in celcius: "))
+print("temperature in Farenheit: ",(temp*1.8)+32)"""
+
+
+#----------------------------------------------------Memory Conversion-----------------------------------------
+
+
+
+GB=int(input("Enter Storage in GB: "))
+print("Stoargae in MB: ",GB*1024)
