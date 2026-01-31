52a57bbfd2ac1cf9264a4bbdf99bbe7d54e8b441
Amish <amishv20@gmail.com>
2026-01-31 09:39:01 +0530

    structured way

---
+++ Basics/Loops/day1.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the number you guessed: "))
+b = int(input("Enter the number that is prize number: "))
+while a != b:
+    print("Wrong")
+    a = int(input("Enter the you guessed: "))
+
+print("You Won!")
